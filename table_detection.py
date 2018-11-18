0b75b1917e43c3c86b5cc979aa5a45e4391fa96e
dibyatanoy <dibyatanoy.bhattacharjee@yale.edu>
2018-11-17 22:46:30 -0500

    [FinalProject] Pocket detection complete, ball detection in progress

--- table_detection.py
+++ table_detection.py
@@ -13,7 +13,10 @@ class TableDetector:
         self.tableEdges = None
         self.tableCorners = None
         self.pockets = None
+        self.balls = None
+        self.tableSurfaceColorRange = (np.array([150, 110, 0], dtype="uint8"), np.array([205, 205, 90], dtype="uint8"))
         self.tableColorRange = (np.array([235, 155, 0], dtype="uint8"), np.array([255, 210, 0], dtype="uint8"))
+        self.pocketColorRange = (np.array([0, 0, 0], dtype="uint8"), np.array([0, 0, 40], dtype="uint8"))
         self.nms_rho_tol = 50
         self.nms_theta_tol = np.pi/180.0 * 30.0
         self.eps = 0.1
@@ -32,6 +35,7 @@ class TableDetector:
             return
         self.image_path = image_path
         self.img = load_game_window(self.image_path)
+        # self.img = cv2.imread(self.image_path, cv2.IMREAD_COLOR)
 
     def __line_non_max_suppression(self, lines, max_size):
         '''
@@ -71,6 +75,53 @@ class TableDetector:
         x0, y0 = int(np.round(x0)), int(np.round(y0))
         return [(x0, y0)]
 
+    def __sort_corners(self, corner_list):
+        print(corner_list)
+        assert len(corner_list) == 4
+
+        sorted_corners = {}
+        corner_list.sort()
+        sorted_corners['tl'] = corner_list[0]
+        sorted_corners['tr'] = corner_list[2]
+        sorted_corners['bl'] = corner_list[1]
+        sorted_corners['br'] = corner_list[-1]
+
+        return sorted_corners
+
+
+    def detect_pockets(self):
+        '''
+        Detect the table pockets. Four of the positions are already known,
+        so only need to detect the 2 middle pockets
+        '''
+
+        mask = cv2.inRange(self.img, self.pocketColorRange[0], self.pocketColorRange[1])
+        params = cv2.SimpleBlobDetector_Params()
+
+        params.filterByArea = True
+        params.maxArea = 10000
+
+        params.filterByCircularity = True
+        params.minCircularity = 0.5
+
+        detector = cv2.SimpleBlobDetector(params)
+        keypoints = detector.detect(255 - mask)
+        filtered_keypoints = []
+        for keypoint in keypoints:
+            if keypoint.pt[0] >= self.tableCorners['tl'][0]-20 and \
+                keypoint.pt[0] < self.tableCorners['br'][0]+20 and \
+                keypoint.pt[1] >= self.tableCorners['tl'][1] and \
+                keypoint.pt[1] < self.tableCorners['br'][1]:
+
+                # could modify this to add projections of points onto edges
+                # instead of the raw points themselves
+                filtered_keypoints.append((int(keypoint.pt[0]), int(keypoint.pt[1])))
+
+        filtered_keypoints.sort()
+        self.pockets = dict(self.tableCorners)
+        self.pockets['ml'] = filtered_keypoints[0]
+        self.pockets['mr'] = filtered_keypoints[1]
+
     def detect_table_edges(self):
         if self.image_path == None:
             __log_error("No image loaded")
@@ -84,28 +135,53 @@ class TableDetector:
 
         # non-max suppression, since lines are in order of confidence
         edge_lines = self.__line_non_max_suppression(lines, 4)
-        self.tableCorners = []
-        for edge1 in edge_lines:
-            for edge2 in edge_lines:
-                self.tableCorners.extend(self.__intersection(edge1, edge2))
-        for corner in self.tableCorners:
-            cv2.circle(image_copy, corner, 10, (0, 255, 0))
-
-        for rho,theta in edge_lines:
-            # https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_imgproc/py_houghlines/py_houghlines.html
-            a = np.cos(theta)
-            b = np.sin(theta)
-            x0 = a*rho
-            y0 = b*rho
-            x1 = int(x0 + 1000*(-b))
-            y1 = int(y0 + 1000*(a))
-            x2 = int(x0 - 1000*(-b))
-            y2 = int(y0 - 1000*(a))
-
-            cv2.line(image_copy,(x1,y1),(x2,y2),(0,0,255),2)
-        
+        table_corners = []
+        for i in range(0, len(edge_lines)):
+            for j in range(i+1, len(edge_lines)):
+                table_corners.extend(self.__intersection(edge_lines[i], edge_lines[j]))
+        self.tableCorners = self.__sort_corners(table_corners)
         self.tableEdges = edge_lines
-        self.__display_image_internal(image_copy, title="Table edges")
+
+    def detect_balls(self):
+        table_crop = self.img[
+            self.tableCorners['tl'][1]:self.tableCorners['bl'][1],
+            self.tableCorners['tl'][0]:self.tableCorners['tr'][0]].copy()
+        mask_lower = 255 - cv2.inRange(table_crop, self.tableSurfaceColorRange[0], self.tableSurfaceColorRange[1])
+        self.__display_image_internal(mask_lower)
+
+
+    def display_table_detections(self):
+        image_copy = np.copy(self.img)
+        if self.tableCorners:
+            tl = self.tableCorners['tl']
+            bl = self.tableCorners['bl']
+            tr = self.tableCorners['tr']
+            br = self.tableCorners['br']
+
+            cv2.line(image_copy, tl, tr, (0, 0, 255), 2)
+            cv2.line(image_copy, tl, bl, (0, 0, 255), 2)
+            cv2.line(image_copy, tr, br, (0, 0, 255), 2)
+            cv2.line(image_copy, bl, br, (0, 0, 255), 2)
+
+        # if self.tableEdges:
+        #     for rho,theta in self.tableEdges:
+        #         # https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_imgproc/py_houghlines/py_houghlines.html
+        #         a = np.cos(theta)
+        #         b = np.sin(theta)
+        #         x0 = a*rho
+        #         y0 = b*rho
+        #         x1 = int(x0 + 1000*(-b))
+        #         y1 = int(y0 + 1000*(a))
+        #         x2 = int(x0 - 1000*(-b))
+        #         y2 = int(y0 - 1000*(a))
+        #
+        #         cv2.line(image_copy,(x1,y1),(x2,y2),(0,0,255),2)
+
+        if self.pockets:
+            for pocket in self.pockets:
+                cv2.circle(image_copy, self.pockets[pocket], 10, (0, 255, 0))
+
+        self.__display_image_internal(image_copy, title="Table detections")
 
     def display_image(self):
         if self.image_path == None:
@@ -119,7 +195,9 @@ class TableDetector:
 def main():
     td = TableDetector()
     td.load_image("sample_screenshot.png")
-    td.detect_table_edges()()
+    td.detect_table_edges()
+    td.detect_pockets()
+    td.detect_balls()
 
 if __name__ == '__main__':
     main()
