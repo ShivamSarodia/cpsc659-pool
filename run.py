a9505cf212c35f889d15546d59f19439b3487750
Shivam Sarodia <ssarodia@gmail.com>
2018-12-02 03:55:00 -0500

    Update run code for multiple detections

--- run.py
+++ run.py
@@ -12,19 +12,28 @@ from player import Player
 current_goal = sys.argv[1]
 
 while True:
-    # Take a screenshot
-    screenshot_name = GameController.get_screen_image()
+    # Get preliminary screenshot.
+    prelim_screenshot = GameController.get_screen_image()
+    prelim_td = TableDetector()
+    prelim_td.load_image(prelim_screenshot)
+    prelim_td.detect_all()
 
-    # Detect objects in the image
+    # Determine where to place the cue for real screenshot.
+    prelim_controller = GameController(
+        prelim_td.tableSize, prelim_td.tableCropTopLeft, prelim_td.balls, prelim_td.ballRadius)
+    stick_target = prelim_controller.find_stick_position()
+    prelim_controller.move_mouse(stick_target)
+
+    # Get real screenshot.
+    screenshot = GameController.get_screen_image()
     td = TableDetector()
-    td.load_image(screenshot_name)
+    td.load_image(screenshot)
     td.detect_all()
+    td.remove_nondup_balls(prelim_td.balls)
 
-    # Create player to determine where to shoot
     player = Player(td.tableSize, td.pockets, td.balls, td.ballRadius, current_goal)
     target, force = player.get_shot(), 1
 
-    # Create game controller
-    controller = GameController(td.tableSize, td.tableCropTopLeft, td.balls["white"])
+    controller = GameController(td.tableSize, td.tableCropTopLeft, td.balls, td.ballRadius)
     controller.make_shot(target, force)
     time.sleep(10)
